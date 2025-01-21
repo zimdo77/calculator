2acacf50f984db404f734db323fd2c7d42f2111b
Zim Do <zimd0110@gmail.com>
2025-01-21 15:15:59 +1100

    create basic operator functions

---
+++ script.js
@@ -0,0 +1,40 @@
+const ADD = 'add';
+const SUBTRACT = 'subtract';
+const MULTIPLY = 'multiply';
+const DIVIDE = 'divide';
+
+
+function add(num1, num2) {
+  return (num1 + num2);
+}
+
+function subtract(num1, num2) {
+  return (num1 - num2);
+}
+
+function multiply(num1, num2) {
+  return (num1 * num2);
+}
+
+function divide(num1, num2) {
+  return (num1 / num2);
+}
+
+function operate(operator, num1, num2) {
+  let result;
+  switch (operator) {
+    case ADD:
+      result = add(num1, num2);
+      break;
+    case SUBTRACT:
+      result = subtract(num1, num2);
+      break;
+    case MULTIPLY:
+      result = multiply(num1, num2);
+      break;
+    case DIVIDE:
+      result = divide(num1, num2);
+      break;
+  }
+  return result;
+}
