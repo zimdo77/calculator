81a2f1ed06d3c4790d8501168618c7624daa5ccb
Zim Do <zimd0110@gmail.com>
2025-01-22 16:20:58 +1100

    add basic calculator layout

--- script.js
+++ script.js
@@ -5,36 +5,36 @@ const DIVIDE = 'divide';
 
 
 function add(num1, num2) {
-  return (num1 + num2);
+    return (num1 + num2);
 }
 
 function subtract(num1, num2) {
-  return (num1 - num2);
+    return (num1 - num2);
 }
 
 function multiply(num1, num2) {
-  return (num1 * num2);
+    return (num1 * num2);
 }
 
 function divide(num1, num2) {
-  return (num1 / num2);
+    return (num1 / num2);
 }
 
 function operate(operator, num1, num2) {
-  let result;
-  switch (operator) {
-    case ADD:
-      result = add(num1, num2);
-      break;
-    case SUBTRACT:
-      result = subtract(num1, num2);
-      break;
-    case MULTIPLY:
-      result = multiply(num1, num2);
-      break;
-    case DIVIDE:
-      result = divide(num1, num2);
-      break;
-  }
+    let result;
+    switch (operator) {
+        case ADD:
+            result = add(num1, num2);
+            break;
+        case SUBTRACT:
+            result = subtract(num1, num2);
+            break;
+        case MULTIPLY:
+            result = multiply(num1, num2);
+            break;
+        case DIVIDE:
+            result = divide(num1, num2);
+            break;
+    }
   return result;
 }
